8cd65708da0d7becd36561133712bc44dfd47357
Krish Singh <singh.krish261108@gmail.com>
2026-09-03 00:51:03 +0530

    Add branded loading state

---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div style={{ minHeight: "100vh", display: "grid", placeItems: "center", background: "#071321", color: "#f3d88a", fontFamily: "Manrope, sans-serif", letterSpacing: ".18em", fontSize: 11 }}>L&apos;ELITE</div>;
+}
